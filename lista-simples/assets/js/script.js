e1be1d54654055c8e8b45223c8b03b0400d4a366
André Marques <andre.m.d.abreu@gmail.com>
2023-05-05 14:37:40 -0300

    finalização do exercicio de lista simples

--- lista-simples/assets/js/script.js
+++ lista-simples/assets/js/script.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+const input = document.querySelector("input");
+
+input.addEventListener("keyup", function(event) {
+    if(event.code == "Enter") {
+        const list = document.querySelector("ul");
+        const listItem = document.createElement("li");
+    
+        listItem.innerHTML = input.value;
+        list.appendChild(listItem);
+        input.value = ""
+    }
+});
